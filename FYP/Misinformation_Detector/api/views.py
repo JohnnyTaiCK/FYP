164245ce2dfe514abda1129bed77250b818aa8aa
JohnnyTaiCK <126487338+JohnnyTaiCK@users.noreply.github.com>
2024-03-10 01:24:16 +0800

    10/3/2024

--- FYP/Misinformation_Detector/api/views.py
+++ FYP/Misinformation_Detector/api/views.py
@@ -3,18 +3,19 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from models.flan_t5 import Flan_T5
 from models.chatgpt import OpenAIModel
+import os
+import openai
 
 # Create your views here.
 class APIViewSet(viewsets.ViewSet):
     
     detect_model = Flan_T5()
-   #explain_model = OpenAIModel(API_KEY="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", model_name="gpt-3.5-turbo", max_new_tokens=1000)
+    # explain_model = OpenAIModel(API_KEY="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", model_name="gpt-3.5-turbo", max_new_tokens=1000)
 
     def create(self,request):
         result = self.detect_model.answer_logics(info=None, gq=request.data.get("news"))
         result_text = ""
 
-        #print(self.explain_model.chat_generate("what is the answer to 1 + 1?"))
         print(request.data.get("news"))
         print(result)
         if(result > 0.5):
@@ -22,3 +23,4 @@ class APIViewSet(viewsets.ViewSet):
         else:
             result_text = "False"
         return Response({'prediction': result_text})
+# %%
